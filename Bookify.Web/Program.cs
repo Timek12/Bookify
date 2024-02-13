2854bacc185ab9411c983e6aa2254fb7166eddce
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-13 22:47:19 +0100

    Add service to connect ApplicationDbContext

--- Bookify.Web/Program.cs
+++ Bookify.Web/Program.cs
@@ -1,7 +1,11 @@
+using Bookify.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
