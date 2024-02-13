ffb84523047ab5a338362110a0b0be72313ea8df
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-13 23:58:33 +0100

    Remove unused project

--- BookifyWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BookifyWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
