32c83b8e592bd95d8d0bb19a9c8cd08920b9ff8d
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-14 01:04:44 +0100

    Create a view for villa list

---
+++ Bookify.Web/Controllers/VillaController.cs
@@ -0,0 +1,25 @@
+﻿using Bookify.Domain.Entities;
+using Bookify.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Runtime.CompilerServices;
+
+namespace Bookify.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var villaList = _db.Villas.ToList();
+            return View(villaList);
+        }
+    }
+}
+
+
