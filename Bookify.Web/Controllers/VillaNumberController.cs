f8a365976d5d90e46431eb25c8082f5fd0ac4ed4
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-15 23:12:06 +0100

    Add endpoint for villa number list

---
+++ Bookify.Web/Controllers/VillaNumberController.cs
@@ -0,0 +1,25 @@
+﻿using Bookify.Domain.Entities;
+using Bookify.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Runtime.CompilerServices;
+
+namespace Bookify.Web.Controllers
+{
+    public class VillaNumberController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public VillaNumberController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var villaNumbers = _db.VillaNumbers.ToList();
+            return View(villaNumbers);
+        }
+    }
+}
+
+
