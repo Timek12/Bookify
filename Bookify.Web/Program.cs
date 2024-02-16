badd1de0436e8804afd012d02b0a489a8631a704
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-16 18:44:40 +0100

    Inject dependency for villa repository

--- Bookify.Web/Program.cs
+++ Bookify.Web/Program.cs
@@ -1,3 +1,4 @@
+using Bookify.Application.Common.Interfaces;
 using Bookify.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddScoped<IVillaRepository, IVillaRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
