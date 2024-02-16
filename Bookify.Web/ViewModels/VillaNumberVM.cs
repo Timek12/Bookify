058db558a6a13243819d7dda2b7aabc845bafe0f
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-16 16:05:08 +0100

    Create villa number view model

---
+++ Bookify.Web/ViewModels/VillaNumberVM.cs
@@ -0,0 +1,13 @@
+﻿using Bookify.Domain.Entities;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Bookify.Web.ViewModels
+{
+    public class VillaNumberVM
+    {
+        public VillaNumber? VillaNumber { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> VillaList { get; set; }
+    }
+}
