259c2f5198797ae7ac91c58e4a3ddea47203c334
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-16 18:33:22 +0100

    Create interface for villa repository

---
+++ Bookify.Application/Common/Interfaces/IVillaRepository.cs
@@ -0,0 +1,21 @@
+﻿using Bookify.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Application.Common.Interfaces
+{
+    public interface IVillaRepository
+    {
+        IEnumerable<Villa> GetAll(Expression<Func<Villa, bool>>? filter = null, string? includeProperties = null);
+        Villa Get(Expression<Func<Villa, bool>> filter,  string? includeProperties = null);
+        void Add(Villa entity);
+        void Update(Villa entity);
+        void Remove(Villa entity);
+        void Save();
+
+    }
+}
