f6d3e42285b12095e7aab4a5fda712aad39b1d52
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-17 13:50:40 +0100

    Implement villa number repository interface

---
+++ Bookify.Application/Common/Interfaces/IVillaNumberRepository.cs
@@ -0,0 +1,15 @@
+﻿using Bookify.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Application.Common.Interfaces
+{
+    public interface IVillaNumberRepository : IRepository<VillaNumber>
+    {
+        void Update(VillaNumber entity);
+        void Save();
+    }
+}
