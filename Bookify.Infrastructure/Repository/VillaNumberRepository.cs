d8efb02feb33cecf52406bd0f9bd5a4b6716bd2c
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-17 13:50:49 +0100

    Implement villa number repository

---
+++ Bookify.Infrastructure/Repository/VillaNumberRepository.cs
@@ -0,0 +1,30 @@
+﻿using Bookify.Application.Common.Interfaces;
+using Bookify.Domain.Entities;
+using Bookify.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Infrastructure.Repository
+{
+    public class VillaNumberRepository : Repository<VillaNumber>, IVillaNumberRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public VillaNumberRepository(ApplicationDbContext db) : base(db) 
+        {
+            _db = db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(VillaNumber entity)
+        {
+            _db.VillaNumbers.Update(entity);
+        }
+    }
+}
