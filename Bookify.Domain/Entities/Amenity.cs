b095a9885877a761ce6f3e2303b8ba52eec2d3c2
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-17 18:26:27 +0100

    Create Amenity model

---
+++ Bookify.Domain/Entities/Amenity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Domain.Entities
+{
+    public class Amenity
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+
+        [ForeignKey("Villa")]
+        public int VillaId { get; set; }
+        [ValidateNever]
+        public Villa Villa { get; set; }
+    }
+}
