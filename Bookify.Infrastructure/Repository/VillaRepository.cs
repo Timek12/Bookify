68e9a14cbe60e362144c8ffa16aa4eb5ad49d637
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-17 01:04:45 +0100

    Update villa repository to inherit from generic repostiory

--- Bookify.Infrastructure/Repository/VillaRepository.cs
+++ Bookify.Infrastructure/Repository/VillaRepository.cs
@@ -11,59 +11,13 @@ using System.Threading.Tasks;
 
 namespace Bookify.Infrastructure.Repository
 {
-    public class VillaRepository : IVillaRepository
+    public class VillaRepository : Repository<Villa>, IVillaRepository
     {
         private readonly ApplicationDbContext _db;
-        public VillaRepository(ApplicationDbContext db)
+        public VillaRepository(ApplicationDbContext db) :base(db)
         {
             _db = db;
         }
-        public void Add(Villa entity)
-        {
-            _db.Add(entity);
-        }
-
-        public Villa? Get(Expression<Func<Villa, bool>> filter, string? includeProperties = null)
-        {
-            IQueryable<Villa> query = _db.Set<Villa>();
-            query = query.Where(filter);
-
-            if (!string.IsNullOrEmpty(includeProperties))
-            {
-                foreach(var property in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
-                {
-                    query = query.Include(property);
-                }
-            }
-
-            return query.FirstOrDefault();
-        }
-
-        public IEnumerable<Villa> GetAll(Expression<Func<Villa, bool>>? filter = null, string? includeProperties = null)
-        {
-            IQueryable<Villa> query = _db.Set<Villa>();
-            if(filter is not null)
-            {
-                query = query.Where(filter);
-            }
-
-            if (!string.IsNullOrEmpty(includeProperties))
-            {
-                foreach(var property in includeProperties
-                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
-                {
-                    query.Include(property);
-                }
-            }
-
-            return query.ToList();
-        }
-
-        public void Remove(Villa entity)
-        {
-            _db.Remove(entity);
-        }
-
         public void Save()
         {
             _db.SaveChanges();
