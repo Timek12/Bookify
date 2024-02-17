b55302769cc6353da5c41b089e19ab349be2fd34
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-17 13:52:23 +0100

    Add villa number repository

--- Bookify.Infrastructure/Repository/UnitOfWork.cs
+++ Bookify.Infrastructure/Repository/UnitOfWork.cs
@@ -12,11 +12,13 @@ namespace Bookify.Infrastructure.Repository
     {
         private readonly ApplicationDbContext _db;
         public IVillaRepository Villa { get; private set; }
+        public IVillaNumberRepository VillaNumber { get; private set; }
 
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;            
             Villa = new VillaRepository(_db);
+            VillaNumber = new VillaNumberRepository(_db);
         }
     }
 }
