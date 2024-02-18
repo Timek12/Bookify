aac31c24754fa6f37c3451ed93f55d47a3237e2e
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-18 23:30:39 +0100

    Create home view model

---
+++ Bookify.Web/ViewModels/HomeVM.cs
@@ -0,0 +1,12 @@
+﻿using Bookify.Domain.Entities;
+
+namespace Bookify.Web.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Villa> VillaList { get; set; }
+        public DateOnly CheckInDate { get; set; }
+        public DateOnly? CheckOutDate { get;set; }
+        public int Nights { get; set; }
+    }
+}
