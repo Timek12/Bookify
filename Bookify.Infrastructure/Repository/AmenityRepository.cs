c47abe16d4b355df465ba510c128afea76df4245
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-18 10:34:01 +0100

    Implement amenity repository

---
+++ Bookify.Infrastructure/Repository/AmenityRepository.cs
@@ -0,0 +1,24 @@
+﻿using Bookify.Application.Common.Interfaces;
+using Bookify.Domain.Entities;
+using Bookify.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Infrastructure.Repository
+{
+    public class AmenityRepository : Repository<Amenity>, IAmenityRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public AmenityRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;            
+        }
+        public void Update(Amenity entity)
+        {
+            _db.Amenities.Update(entity);
+        }
+    }
+}
