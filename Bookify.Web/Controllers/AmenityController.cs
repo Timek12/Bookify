9d771d65c508bca2d46fc829c16c95f51e217b29
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-18 10:52:02 +0100

    Add post endpoint for amenity list UI

---
+++ Bookify.Web/Controllers/AmenityController.cs
@@ -0,0 +1,23 @@
+﻿using Bookify.Application.Common.Interfaces;
+using Bookify.Domain.Entities;
+using Bookify.Infrastructure.Data;
+using Bookify.Infrastructure.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookify.Web.Controllers
+{
+    public class AmenityController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public AmenityController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;           
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Amenity> amenityList = _unitOfWork.Amenity.GetAll();
+            return View(amenityList);
+        }
+    }
+}
