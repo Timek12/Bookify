8602ff521ff025f52fd552433f0ba7ef65928b70
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-19 15:05:56 +0100

    Add custom application users table

--- Bookify.Infrastructure/Data/ApplicationDbContext.cs
+++ Bookify.Infrastructure/Data/ApplicationDbContext.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Bookify.Infrastructure.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -20,6 +20,8 @@ namespace Bookify.Infrastructure.Data
         public DbSet<Villa> Villas { get; set; }
         public DbSet<VillaNumber> VillaNumbers { get; set; }
         public DbSet<Amenity> Amenities { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
