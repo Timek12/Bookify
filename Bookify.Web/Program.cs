08fa921f24cc4af5ce5072f220957bf361f8c650
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-19 15:06:15 +0100

    Change identity user to custom application user

--- Bookify.Web/Program.cs
+++ Bookify.Web/Program.cs
@@ -1,4 +1,5 @@
 using Bookify.Application.Common.Interfaces;
+using Bookify.Domain.Entities;
 using Bookify.Infrastructure.Data;
 using Bookify.Infrastructure.Repository;
 using Microsoft.AspNetCore.Identity;
@@ -9,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
