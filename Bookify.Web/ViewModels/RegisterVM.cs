072b896292ed062d269ed0197a27d50f5df6acd6
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-19 15:26:48 +0100

    Add view models for login and register

---
+++ Bookify.Web/ViewModels/RegisterVM.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bookify.Web.ViewModels
+{
+    public class RegisterVM
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        [DisplayName("Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [DisplayName("Phone Number")]
+        public string? PhoneNumber { get; set; }
+        public string? RedirectUrl { get; set; }
+    }
+}
