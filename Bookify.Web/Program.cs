05032aea18245321f16e24b81b3ac573356c10ae
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-19 14:49:21 +0100

    Add identity configuration

--- Bookify.Web/Program.cs
+++ Bookify.Web/Program.cs
@@ -1,6 +1,7 @@
 using Bookify.Application.Common.Interfaces;
 using Bookify.Infrastructure.Data;
 using Bookify.Infrastructure.Repository;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
