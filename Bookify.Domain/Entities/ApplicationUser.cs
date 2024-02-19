a9cb105e926e268c7f9a3222da25526d5de3df8d
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-19 15:04:51 +0100

    Create identity user

---
+++ Bookify.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Bookify.Domain.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
