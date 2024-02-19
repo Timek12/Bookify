fd17cb9c300a5774d94d2087623170cbfe788b5b
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-19 16:02:04 +0100

    Add properties and create constructor

---
+++ Bookify.Web/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using Bookify.Application.Common.Interfaces;
+using Bookify.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Runtime.CompilerServices;
+
+namespace Bookify.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public AccountController(IUnitOfWork unitOfWork, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+    
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+
+}
+
+
+
