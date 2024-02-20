dabd6ccd2eb24cde61a6cf681d4700b996ce51b0
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-20 17:39:31 +0100

    Add temporary post endpoint for setting availability of the villa

--- Bookify.Web/Controllers/HomeController.cs
+++ Bookify.Web/Controllers/HomeController.cs
@@ -26,6 +26,21 @@ namespace Bookify.Web.Controllers
             return View(homeVM);
         }
 
+        [HttpPost]
+        public IActionResult Index(HomeVM homeVM)
+        {
+            homeVM.VillaList = _unitOfWork.Villa.GetAll(includeProperties: "AmenityList");
+            foreach(var villa in homeVM.VillaList)
+            {
+                if(villa.Id % 2 == 0)
+                {
+                    villa.IsAvailable = false;
+                }
+            }
+
+            return View(homeVM);
+        }
+
         public IActionResult Privacy()
         {
             return View();
