80b3514b662000517dad411d1d596c93644cd888
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-21 00:09:06 +0100

    Add booking interface repository

---
+++ Bookify.Application/Common/Interfaces/IBookingRepository.cs
@@ -0,0 +1,14 @@
+﻿using Bookify.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Application.Common.Interfaces
+{
+    public interface IBookingRepository : IRepository<Booking>
+    {
+        public void Update();
+    }
+}
