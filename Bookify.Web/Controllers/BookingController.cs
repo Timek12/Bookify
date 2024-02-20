12bbe482bfd8e16308beceb49be227be87e3dbee
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-21 00:19:57 +0100

    Create new booking controller, add get endpoint for finalizing booking

---
+++ Bookify.Web/Controllers/BookingController.cs
@@ -0,0 +1,27 @@
+﻿using Bookify.Application.Common.Interfaces;
+using Bookify.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookify.Web.Controllers
+{
+    public class BookingController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public BookingController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult FinalizeBooking(int villaId, DateOnly checkInDate, int nights)
+        {
+            Booking booking = new()
+            {
+                VillaId = villaId,
+                Villa = _unitOfWork.Villa.Get(u => u.Id == villaId, includeProperties: "AmenityList"),
+                CheckInDate = checkInDate,
+                Nights = nights,
+                CheckOutDate = checkInDate.AddDays(nights),
+            };
+            return View();
+        }
+    }
+}
