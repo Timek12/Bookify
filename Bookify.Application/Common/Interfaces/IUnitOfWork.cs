6b673ff75882fa7798c647e423a57d84a1a2c63c
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-21 16:20:38 +0100

    Add application user inyterface

--- Bookify.Application/Common/Interfaces/IUnitOfWork.cs
+++ Bookify.Application/Common/Interfaces/IUnitOfWork.cs
@@ -12,6 +12,7 @@ namespace Bookify.Application.Common.Interfaces
         IVillaNumberRepository VillaNumber { get; }
         IAmenityRepository Amenity { get; }
         IBookingRepository Booking { get; }
+        IApplicationUserRepository ApplicationUser { get; }
         void Save();
     }
 }
