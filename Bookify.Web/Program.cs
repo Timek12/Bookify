b5d7553400241e9850a9de3930a203feeabf1926
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-22 00:32:36 +0100

    Add stripe configuration api key

--- Bookify.Web/Program.cs
+++ Bookify.Web/Program.cs
@@ -4,6 +4,7 @@ using Bookify.Infrastructure.Data;
 using Bookify.Infrastructure.Repository;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Stripe;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,6 +27,7 @@ builder.Services.Configure<IdentityOptions>(option =>
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
+StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
