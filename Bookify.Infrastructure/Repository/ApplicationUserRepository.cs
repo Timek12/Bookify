103313e6e7baf0ca369b3490598fb1e8a5bccda7
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-21 16:54:24 +0100

    Create application user repo

---
+++ Bookify.Infrastructure/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using Bookify.Application.Common.Interfaces;
+using Bookify.Domain.Entities;
+using Bookify.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Infrastructure.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
