4f4957446cdbb6d9ffe2dfcf7a4bb16117d5aae3
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-21 17:12:44 +0100

    Add user properties to booking object, fill user fields for finalize booking UI

--- Bookify.Web/Controllers/BookingController.cs
+++ Bookify.Web/Controllers/BookingController.cs
@@ -1,6 +1,8 @@
 ﻿using Bookify.Application.Common.Interfaces;
 using Bookify.Domain.Entities;
+ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace Bookify.Web.Controllers
 {
@@ -11,8 +13,14 @@ namespace Bookify.Web.Controllers
         {
             _unitOfWork = unitOfWork;
         }
+        [Authorize]
         public IActionResult FinalizeBooking(int villaId, DateOnly checkInDate, int nights)
         {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ApplicationUser user = _unitOfWork.User.Get(u => u.Id == userId);
+
             Booking booking = new()
             {
                 VillaId = villaId,
@@ -20,6 +28,10 @@ namespace Bookify.Web.Controllers
                 CheckInDate = checkInDate,
                 Nights = nights,
                 CheckOutDate = checkInDate.AddDays(nights),
+                UserId = user.Id,
+                PhoneNumber = user.PhoneNumber,
+                Email = user.Email,
+                Name = user.Name
             };
 
             booking.TotalCost = booking.Villa.Price * booking.Nights;
