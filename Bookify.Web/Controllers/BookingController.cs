c415c89f93062fe80f933eb03a51ccd89cf6d58b
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-22 13:24:00 +0100

    Add region for api call to retrive data of all the bookings

--- Bookify.Web/Controllers/BookingController.cs
+++ Bookify.Web/Controllers/BookingController.cs
@@ -120,5 +120,32 @@ namespace Bookify.Web.Controllers
             }
             return View(bookingId);
         }
+
+
+        #region API Calls
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetAll()
+        {
+            IEnumerable<Booking> bookings;
+
+            if(User.IsInRole(SD.Role_Admin))
+            {
+                bookings = _unitOfWork.Booking.GetAll(includeProperties: "User, Villa");
+            }
+            else
+            {
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+                bookings = _unitOfWork.Booking.GetAll(u => u.UserId == userId, includeProperties: "User, Villa");
+            }
+
+            return Json(new { data = bookings });
+        }
+
+        #endregion
+
     }
 }
