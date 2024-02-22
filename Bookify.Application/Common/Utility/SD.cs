c9b5ce17e7de6aa81f72f2d21fcd8a39909b55b2
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-22 17:35:51 +0100

    Add functionality for calculating how many villa rooms are available in a specific date

--- Bookify.Application/Common/Utility/SD.cs
+++ Bookify.Application/Common/Utility/SD.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bookify.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,5 +19,32 @@ namespace Bookify.Application.Common.Utility
         public const string StatusCancelled = "Cancelled";
         public const string StatusRefunded = "Refunded";
 
+        public static int VillaRoomsAvailable_Count(int villaId,
+            List<VillaNumber> villaNumberList, DateOnly checkInDate, int nights,
+            List<Booking> bookings)
+        {
+            List<int> bookingInDate = new();
+
+            var roomsInVilla = villaNumberList.Where(u => u.VillaId == villaId).Count();
+
+            for(int i=0; i<nights; i++)
+            {
+                var villasBooked = bookings.Where(u => u.CheckInDate <= checkInDate.AddDays(i) && u.CheckOutDate > checkInDate.AddDays(i) && u.VillaId == villaId);
+
+                foreach(var booking in villasBooked)
+                {
+                    if (!bookingInDate.Contains(booking.Id))
+                    {
+                        bookingInDate.Add(booking.Id);
+                    }
+                }
+
+                var totalAvailableRooms = roomsInVilla - bookingInDate.Count();
+                if(totalAvailableRooms == 0)
+                {
+                    return 0;
+                }
+            }
+        }
     }
 }
