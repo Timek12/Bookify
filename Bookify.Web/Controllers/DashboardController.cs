2ffac7f1f7cb446fbab59ff375c79b3ae29970a8
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-23 14:32:32 +0100

    Add functionality to calculate bookings in current and previous month

--- Bookify.Web/Controllers/DashboardController.cs
+++ Bookify.Web/Controllers/DashboardController.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bookify.Application.Common.Interfaces;
+using Bookify.Application.Common.Utility;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bookify.Web.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        static int previousMonth = DateTime.Now.Month == 1 ? 12 : DateTime.Now.Month - 1; 
+        readonly DateTime previousMonthStartDate = new(DateTime.Now.Year, previousMonth, 1);
+        readonly DateTime currentMonthStartDate = new(DateTime.Now.Year, DateTime.Now.Month, 1);
+
+        public DashboardController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> GetRadialChartData()
+        {
+            var totalBookings = _unitOfWork.Booking.GetAll(u => u.Status != SD.StatusPending && u.Status != SD.StatusPending);
+            
+            var countByCurrentMonth = totalBookings.Count(u => u.BookingDate >= currentMonthStartDate && u.BookingDate <= DateTime.Now);
+
+            var countByPreviousMonth = totalBookings.Count(u => u.BookingDate >= previousMonthStartDate && u.BookingDate <= currentMonthStartDate);
+
         }
     }
 }
