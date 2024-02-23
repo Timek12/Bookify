e2bb3dbc2fa400643d9b7d73674f371e27c039b2
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-23 16:28:48 +0100

    Move script for loading radial bar chart to separate file

--- Bookify.Web/wwwroot/js/dashboard/getTotalBookingsRadial.js
+++ Bookify.Web/wwwroot/js/dashboard/getTotalBookingsRadial.js
@@ -1,5 +1,4 @@
-﻿
-$(document).ready(function () {
+﻿$(document).ready(function () {
     loadTotalBookingRadialChart();
 })
 
@@ -33,34 +32,3 @@ function loadTotalBookingRadialChart() {
     })
 }
 
-function loadRadialBarChart(chartId, data) {
-    var options = {
-        chart: {
-            height: 100,
-            width: 100,
-            type: "radialBar",
-            sparkline: {
-                enabled: true
-            },
-            offsetY: -10,
-        },
-
-        series: data.series,
-
-        plotOptions: {
-            radialBar: {
-                dataLabels: {
-                    value: {
-                        offsetY: -10,
-                    }
-                }
-            }
-        },
-        labels: [""],
-    };
-
-    var chart = new ApexCharts(document.querySelector("#" + chartId), options);
-
-    chart.render();
-
-}
---
+++ Bookify.Web/wwwroot/js/dashboard/radialChart.js
@@ -0,0 +1,31 @@
+﻿function loadRadialBarChart(chartId, data) {
+    var options = {
+        chart: {
+            height: 100,
+            width: 100,
+            type: "radialBar",
+            sparkline: {
+                enabled: true
+            },
+            offsetY: -10,
+        },
+
+        series: data.series,
+
+        plotOptions: {
+            radialBar: {
+                dataLabels: {
+                    value: {
+                        offsetY: -10,
+                    }
+                }
+            }
+        },
+        labels: [""],
+    };
+
+    var chart = new ApexCharts(document.querySelector("#" + chartId), options);
+
+    chart.render();
+
+}
