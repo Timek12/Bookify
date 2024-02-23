146816a0f8ddf2b7bcc46a469c3201141009a5a4
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-23 14:13:52 +0100

    Add radial bar chart view model

---
+++ Bookify.Web/ViewModels/RadialBarChartVM.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Bookify.Web.ViewModels
+{
+    public class RadialBarChartVM
+    {
+        public decimal TotalCount { get; set; }
+        public decimal IncreaseDecreaseAmount { get; set; }
+        public bool hasRatioIncreased { get; set; }
+        public int[] Series { get; set; }
+    }
+} 
