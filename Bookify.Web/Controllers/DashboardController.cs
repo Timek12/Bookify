8fe90f5567b3e055d56e3350db6ff223cfec45b0
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-23 15:27:25 +0100

    Implement endpoint for getting total bookings for radial chart data

--- Bookify.Web/Controllers/DashboardController.cs
+++ Bookify.Web/Controllers/DashboardController.cs
@@ -1,5 +1,6 @@
 ﻿using Bookify.Application.Common.Interfaces;
 using Bookify.Application.Common.Utility;
+using Bookify.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bookify.Web.Controllers
@@ -8,7 +9,7 @@ namespace Bookify.Web.Controllers
     {
         private readonly IUnitOfWork _unitOfWork;
 
-        static int previousMonth = DateTime.Now.Month == 1 ? 12 : DateTime.Now.Month - 1; 
+        static int previousMonth = DateTime.Now.Month == 1 ? 12 : DateTime.Now.Month - 1;
         readonly DateTime previousMonthStartDate = new(DateTime.Now.Year, previousMonth, 1);
         readonly DateTime currentMonthStartDate = new(DateTime.Now.Year, DateTime.Now.Month, 1);
 
@@ -22,13 +23,30 @@ namespace Bookify.Web.Controllers
             return View();
         }
 
-        public async Task<IActionResult> GetRadialChartData()
+        public async Task<IActionResult> GetTotalBookingRadialChartData()
         {
             var totalBookings = _unitOfWork.Booking.GetAll(u => u.Status != SD.StatusPending && u.Status != SD.StatusPending);
-            
+
             var countByCurrentMonth = totalBookings.Count(u => u.BookingDate >= currentMonthStartDate && u.BookingDate <= DateTime.Now);
 
             var countByPreviousMonth = totalBookings.Count(u => u.BookingDate >= previousMonthStartDate && u.BookingDate <= currentMonthStartDate);
+
+            int increaseDecreaseRatio = 100;
+
+            if(countByPreviousMonth != 0)
+            {
+                increaseDecreaseRatio = Convert.ToInt32((countByCurrentMonth - countByPreviousMonth) / countByPreviousMonth * 100);
+            }
+
+            RadialBarChartVM radialBarChartVM = new()
+            {
+                TotalCount = totalBookings.Count(),
+                CountInCurrentMonth = countByCurrentMonth,
+                HasRatioIncreased = countByCurrentMonth > countByPreviousMonth,
+                Series = new int[] { increaseDecreaseRatio }
+            };
+
+            return Json(radialBarChartVM);
 
         }
     }
