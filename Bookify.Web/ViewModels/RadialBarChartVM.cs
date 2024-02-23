d66ba114e2c4091f058302d0feb7ce3b38411f01
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-23 14:38:35 +0100

    Change name to be more meaningful

--- Bookify.Web/ViewModels/RadialBarChartVM.cs
+++ Bookify.Web/ViewModels/RadialBarChartVM.cs
@@ -5,7 +5,7 @@ namespace Bookify.Web.ViewModels
     public class RadialBarChartVM
     {
         public decimal TotalCount { get; set; }
-        public decimal IncreaseDecreaseAmount { get; set; }
+        public decimal CountInCurrentMonth { get; set; }
         public bool HasRatioIncreased { get; set; }
         public int[] Series { get; set; }
     }
