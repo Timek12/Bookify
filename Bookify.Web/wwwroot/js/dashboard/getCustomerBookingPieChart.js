5a31b932e7ec36a1d9f4950b39b092dcce9db088
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-24 15:10:02 +0100

    Add functionality to laod pie chart

---
+++ Bookify.Web/wwwroot/js/dashboard/getCustomerBookingPieChart.js
@@ -0,0 +1,47 @@
+﻿$(document).ready(function () {
+    loadCustomerBookingPieChart();
+})
+
+function loadCustomerBookingPieChart() {
+    $(".chart-spinner").show();
+
+    $.ajax({
+        url: "/Dashboard/GetBookigPieChartData",
+        type: "GET",
+        data: 'json',
+        success: function (data) {
+
+            loadPieChart("customerBookingsPieChart", data);
+
+            $(".chart-spinner").hide();
+        }
+    })
+}
+
+function loadPieChart(chartId, data) {
+    var chartColors = getChartColorsArray(chartId);
+
+    var options = {
+        series: data.series,
+        labels: data.labels,
+        colors: chartColors,
+        chart: {
+            type: 'pie',
+            width: 380
+        },
+        stroke: {
+            show: false
+        },
+        legend: {
+            position: 'bottom',
+            horizontalAlign: 'center',
+            labels: {
+                colors: "#fff",
+                userSeriesColors: true
+            },
+        },
+    };
+
+    var chart = new ApexCharts(document.querySelector("#" + chartId), options);
+    chart.render();
+}
