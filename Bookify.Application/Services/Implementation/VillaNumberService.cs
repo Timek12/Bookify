8a94398e7628ae77cde459ed62fbe44259c97643
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-25 15:02:20 +0100

    Add villa number service funcitonality

---
+++ Bookify.Application/Services/Implementation/VillaNumberService.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bookify.Application.Common.Interfaces;
+using Bookify.Application.Services.Interface;
+using Bookify.Domain.Entities;
+
+namespace Bookify.Application.Services.Implementation
+{
+    public class VillaNumberService : IVillaNumberService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public VillaNumberService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public void CreateVillaNumber(VillaNumber villaNumber)
+        {
+            _unitOfWork.VillaNumber.Add(villaNumber);
+            _unitOfWork.Save();
+        }
+
+        public bool DeleteVillaNumber(int id)
+        {
+            try
+            {
+                VillaNumber? villaNumberFromDb = _unitOfWork.VillaNumber.Get(u => u.Villa_Number == id);
+                if (villaNumberFromDb is not null)
+                {
+                    _unitOfWork.VillaNumber.Remove(villaNumberFromDb);
+                    _unitOfWork.Save();
+                }
+
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public IEnumerable<VillaNumber> GetAllVillaNumbers()
+        {
+            return _unitOfWork.VillaNumber.GetAll();
+        }
+
+        public VillaNumber GetVillaNumberById(int id)
+        {
+            return _unitOfWork.VillaNumber.Get(u => u.Villa_Number == id);
+        }
+
+        public void UpdateVillaNumber(VillaNumber villaNumber)
+        {
+            _unitOfWork.VillaNumber.Update(villaNumber);
+            _unitOfWork.Save();
+        }
+    }
+}
---
+++ Bookify.Application/Services/Interface/IVillaNumberService.cs
@@ -0,0 +1,18 @@
+﻿using Bookify.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Application.Services.Interface
+{
+    public interface IVillaNumberService
+    {
+        IEnumerable<VillaNumber> GetAllVillaNumbers();
+        VillaNumber GetVillaNumberById(int id);
+        void CreateVillaNumber(VillaNumber villaNumber);
+        void UpdateVillaNumber(VillaNumber villaNumber);
+        bool DeleteVillaNumber(int id);
+    }
+}
