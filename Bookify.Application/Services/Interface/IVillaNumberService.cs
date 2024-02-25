cfd03b96715d6da4b7af4f6cea7618d9362c8869
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-25 15:47:48 +0100

    Add villa number service interface

--- Bookify.Application/Services/Interface/IVillaNumberService.cs
+++ Bookify.Application/Services/Interface/IVillaNumberService.cs
@@ -9,10 +9,12 @@ namespace Bookify.Application.Services.Interface
 {
     public interface IVillaNumberService
     {
-        IEnumerable<VillaNumber> GetAllVillaNumbers();
-        VillaNumber GetVillaNumberById(int id);
+        IEnumerable<VillaNumber> GetAllVillaNumbers(string? includeProperty = null);
+        VillaNumber GetVillaNumberById(int id, string? includeProperty = null);
         void CreateVillaNumber(VillaNumber villaNumber);
         void UpdateVillaNumber(VillaNumber villaNumber);
         bool DeleteVillaNumber(int id);
+
+        bool CheckVilaNumberExists(int villa_Number);
     }
 }
