04a46d718826faecbff4e9ad11cf982be9bed0f8
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-25 01:16:34 +0100

    Add endpoint for generating pptx document with villa details

--- Bookify.Web/Controllers/HomeController.cs
+++ Bookify.Web/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using Bookify.Application.Common.Utility;
 using Bookify.Web.Models;
 using Bookify.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Syncfusion.Presentation;
 using System.Diagnostics;
 
 namespace Bookify.Web.Controllers
@@ -10,9 +12,12 @@ namespace Bookify.Web.Controllers
     public class HomeController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public HomeController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -49,6 +54,59 @@ namespace Bookify.Web.Controllers
             };
 
             return PartialView("_VillaList", homeVM);
+        }
+
+        [HttpPost]
+        public IActionResult GeneratePPTExport(int id)
+        {
+            var villa = _unitOfWork.Villa.GetAll(includeProperties: "AmenityList").FirstOrDefault(u => u.Id == id);
+            if(villa is null)
+            {
+                return RedirectToAction(nameof(Error));
+            }
+
+            string basePath = _webHostEnvironment.WebRootPath;
+            string filePath = basePath + @"/Exports/ExportVillaDetails.pptx";
+
+            using IPresentation presentation = Presentation.Open(filePath);
+
+            ISlide slide = presentation.Slides[0];
+
+            IShape? shape = slide.Shapes.FirstOrDefault(u => u.ShapeName == "txtVillaName") as IShape;
+            if(shape is not null)
+            {
+                shape.TextBody.Text = villa.Name;
+            }
+
+            shape = slide.Shapes.FirstOrDefault(u => u.ShapeName == "txtVillaDescription") as IShape;
+            if (shape is not null)
+            {
+                shape.TextBody.Text = villa.Description;
+            }
+
+            shape = slide.Shapes.FirstOrDefault(u => u.ShapeName == "txtOccupancy") as IShape;
+            if (shape is not null)
+            {
+                shape.TextBody.Text = string.Format("Max Occupancy: {0} adults", villa.Occupancy);
+            }
+
+            shape = slide.Shapes.FirstOrDefault(u => u.ShapeName == "txtVillaSize") as IShape;
+            if (shape is not null)
+            {
+                shape.TextBody.Text = string.Format("Villa Size: {0} sqft", villa.Sqft);
+            }
+
+            shape = slide.Shapes.FirstOrDefault(u => u.ShapeName == "txtPricePerNight") as IShape;
+            if (shape is not null)
+            {
+                shape.TextBody.Text = string.Format("Price: {0}/night", villa.Price.ToString("c"));
+            }
+
+            MemoryStream memoryStream = new();
+            presentation.Save(memoryStream);
+            memoryStream.Position = 0;
+            return File(memoryStream, "applicaiton/pptx", "VillaDetails.pptx");
+
         }
 
         public IActionResult Privacy()
