3dd0df3bd13c6afcd484a398a84146bf7721c6a5
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-25 16:09:32 +0100

    Create amenity service interface

---
+++ Bookify.Application/Services/Interface/IAmenityService.cs
@@ -0,0 +1,18 @@
+﻿using Bookify.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Application.Services.Interface
+{
+    public interface IAmenityService
+    {
+        IEnumerable<Amenity> GetAllAmenities(string? includeProperty = null);
+        Amenity GetAmenityById(int id, string? includeProperty = null);
+        void CreateAmenity(Amenity amenity);
+        void UpdateAmenity(Amenity amenity);
+        bool DeleteAmenity(int id);
+    }
+}
