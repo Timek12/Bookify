4aebac1fb9d3e3cce3da62bfbfb0debe7ff15b16
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-25 16:10:42 +0100

    Add dependency injection for amenity service wrapper

--- Bookify.Web/Program.cs
+++ Bookify.Web/Program.cs
@@ -33,6 +33,7 @@ builder.Services.AddScoped<IDbInitializer, DbInitializer>();
 
 builder.Services.AddScoped<IVillaService, VillaService>();
 builder.Services.AddScoped<IVillaNumberService, VillaNumberService>();
+builder.Services.AddScoped<IAmenityService, AmenityService>();
 
 var app = builder.Build();
 
