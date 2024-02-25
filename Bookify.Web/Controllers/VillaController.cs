d23b90ddf18337f3b9b20c47ea40e1b69fc28404
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-25 14:43:57 +0100

    Refactor villa controller to use villa service application layer

--- Bookify.Web/Controllers/VillaController.cs
+++ Bookify.Web/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
 ﻿using Bookify.Application.Common.Interfaces;
+using Bookify.Application.Services.Interface;
 using Bookify.Domain.Entities;
 using Bookify.Infrastructure.Data;
 using Microsoft.AspNetCore.Authorization;
@@ -11,16 +12,15 @@ namespace Bookify.Web.Controllers
     [Authorize]
     public class VillaController : Controller
     {
-        private readonly IUnitOfWork _unitOfWork;
-        private readonly IWebHostEnvironment _webHostEnvironment;
-        public VillaController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+        private readonly IVillaService _villaService;
+        public VillaController(IVillaService villaService)
         {
-            _unitOfWork = unitOfWork;
-            _webHostEnvironment = webHostEnvironment;
+            _villaService = villaService;
         }
+
         public IActionResult Index()
         {
-            var villaList = _unitOfWork.Villa.GetAll();
+            var villaList = _villaService.GetAllVillas();
             return View(villaList);
         }
 
@@ -34,23 +34,8 @@ namespace Bookify.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                if (villa.Image is not null)
-                {
-                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(villa.Image.FileName);
-                    string imagePath = Path.Combine(_webHostEnvironment.WebRootPath, @"images\VillaImage");
+                _villaService.CreateVilla(villa);
 
-                    using var fileStream = new FileStream(Path.Combine(imagePath, fileName), FileMode.Create);
-                    villa.Image.CopyTo(fileStream);
-
-                    villa.ImageUrl = @"\images\VillaImage\" + fileName;
-                }
-                else
-                {
-                    villa.ImageUrl = "https://placehold.co/600x400";
-                }
-
-                _unitOfWork.Villa.Add(villa);
-                _unitOfWork.Save();
                 TempData["success"] = "The villa has been created successfully!";
                 return RedirectToAction(nameof(Index));
             }
@@ -59,14 +44,14 @@ namespace Bookify.Web.Controllers
             return View(villa);
         }
 
-        public IActionResult Update(int? villaId)
+        public IActionResult Update(int villaId)
         {
-            if (villaId == 0 || villaId is null)
+            if (villaId == 0)
             {
                 return RedirectToAction("Error", "Home");
             }
 
-            Villa? villa = _unitOfWork.Villa.Get(u => u.Id == villaId);
+            Villa? villa = _villaService.GetVillaById(villaId);
             if (villa is null)
             {
                 return RedirectToAction("Error", "Home");
@@ -80,30 +65,8 @@ namespace Bookify.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                if (villa.Image is not null)
-                {
-                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(villa.Image.FileName);
-                    string imagePath = Path.Combine(_webHostEnvironment.WebRootPath, @"images\VillaImage");
+                _villaService.UpdateVilla(villa);
 
-                    if (!string.IsNullOrEmpty(villa.ImageUrl))
-                    {
-                        var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, villa.ImageUrl.TrimStart('\\'));
-
-                        if (System.IO.File.Exists(oldImagePath))
-                        {
-                            System.IO.File.Delete(oldImagePath);
-                        }
-                    }
-
-                    using var fileStream = new FileStream(Path.Combine(imagePath, fileName), FileMode.Create);
-                    villa.Image.CopyTo(fileStream);
-
-                    villa.ImageUrl = @"\images\VillaImage\" + fileName;
-                }
-
-
-                _unitOfWork.Villa.Update(villa);
-                _unitOfWork.Save();
                 TempData["success"] = "The villa has been updated successfully!";
                 return RedirectToAction(nameof(Index));
             }
@@ -112,14 +75,14 @@ namespace Bookify.Web.Controllers
             return View(villa);
         }
 
-        public IActionResult Delete(int? villaId)
+        public IActionResult Delete(int villaId)
         {
-            if (villaId == 0 || villaId is null)
+            if (villaId == 0)
             {
                 return RedirectToAction("Error", "Home");
             }
 
-            Villa? villa = _unitOfWork.Villa.Get(u => u.Id == villaId);
+            Villa? villa = _villaService.GetVillaById(villaId);
             if (villa == null)
             {
                 return RedirectToAction("Error", "Home");
@@ -131,25 +94,13 @@ namespace Bookify.Web.Controllers
         [HttpPost]
         public IActionResult Delete(Villa villa)
         {
-            Villa? villaFromDb = _unitOfWork.Villa.Get(u => u.Id == villa.Id);
-            if (villaFromDb is not null)
+            bool isVillaDeleted = _villaService.DeleteVilla(villa.Id);
+            if(isVillaDeleted)
             {
-                if (!string.IsNullOrEmpty(villaFromDb.ImageUrl))
-                {
-                    var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, villaFromDb.ImageUrl.TrimStart('\\'));
-
-                    if (System.IO.File.Exists(oldImagePath))
-                    {
-                        System.IO.File.Delete(oldImagePath);
-                    }
-                }
-             
-                _unitOfWork.Villa.Remove(villaFromDb);
-                _unitOfWork.Save();
                 TempData["success"] = "The villa has been deleted successfully!";
                 return RedirectToAction(nameof(Index));
             }
-
+               
             TempData["error"] = "The villa could not be deleted.";
             return View(villa);
         }
